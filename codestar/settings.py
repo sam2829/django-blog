176b87a853ee8b38161abae70236d38f0ab93ee3
sam2829 <>
2023-10-15 15:41:38 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['i-think-therefore-i-blog-28-75117b6e2bd4.herokuapp.com',
                  '8000-sam2829-django-blog-92ahs0jwgx.us2.codeanyapp.com']
